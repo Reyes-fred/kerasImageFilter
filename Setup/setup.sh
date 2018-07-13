703372d43dabe7602a7bd3a1e5612169c0c71b88
Alfredo <reyes-fred@hotmail.com>
2018-07-13 10:10:34 -0500

    Update

--- Setup/setup.sh
+++ Setup/setup.sh
@@ -1,1 +1,5 @@
-#
+sudo apt-get install python-numpy
+sudo python3 -m pip install numpy
+sudo python3 -m pip install pandas
+sudo python3 -m pip install keras
+sudo python3 -m pip install tensorflow
--- main.py
+++ main.py
@@ -9,30 +9,33 @@ from keras.layers import Input
 from scipy.optimize import fmin_l_bfgs_b
 import time
 
-cImPath = '/home/amiko/Desktop/ImagesKeras/base.jpg' #Base Image
-sImPath = '/home/amiko/Desktop/ImagesKeras/filter.jpg' #Style Image
-genImOutPath = '/home/amiko/Desktop/ImagesKeras/out.jpg' #Image generated
+#Path images
 
+basePath = '/home/amiko/Desktop/ImagesKeras/base.jpg'   #path base image
+filterPath = '/home/amiko/Desktop/ImagesKeras/filter.jpg'  #path filter image
+resultPath = '/home/amiko/Desktop/ImagesKeras/final.jpg' #path result image
+
+# Image processing
 targetHeight = 512
 targetWidth = 512
 targetSize = (targetHeight, targetWidth)
 
-cImageOrig = Image.open(cImPath)
-cImageSizeOrig = cImageOrig.size
-cImage = load_img(path=cImPath, target_size=targetSize)
-cImArr = img_to_array(cImage)
-cImArr = K.variable(preprocess_input(np.expand_dims(cImArr, axis=0)), dtype='float32')
+baseImageOrig = Image.open(basePath)
+baseImageSizeOrig = baseImageOrig.size
+baseImage = load_img(path=basePath, target_size=targetSize)
+baseImArr = img_to_array(baseImage)
+baseImArr = K.variable(preprocess_input(np.expand_dims(baseImArr, axis=0)), dtype='float32')
 
-sImage = load_img(path=sImPath, target_size=targetSize)
-sImArr = img_to_array(sImage)
-sImArr = K.variable(preprocess_input(np.expand_dims(sImArr, axis=0)), dtype='float32')
+filterImage = load_img(path=filterPath, target_size=targetSize)
+filterImArr = img_to_array(filterImage)
+filterImArr = K.variable(preprocess_input(np.expand_dims(filterImArr, axis=0)), dtype='float32')
 
-gIm0 = np.random.randint(256, size=(targetWidth, targetHeight,3)).astype('float64')
+gIm0 = np.random.randint(256, size=(targetWidth, targetHeight, 3)).astype('float64')
 gIm0 = preprocess_input(np.expand_dims(gIm0, axis=0))
 
 gImPlaceholder = K.placeholder(shape=(1, targetWidth, targetHeight, 3))
 
-#Define of helpers functions
+# Definition of the loss and helper functions
 
 def get_feature_reps(x, layer_names, model):
     featMatrices = []
@@ -40,9 +43,9 @@ def get_feature_reps(x, layer_names, model):
         selectedLayer = model.get_layer(ln)
         featRaw = selectedLayer.output
         featRawShape = K.shape(featRaw).eval(session=tf_session)
-        N_1 = featRawShape[-1]
-        M_1 = featRawShape[1]*featRawShape[2]
-        featMatrix = K.reshape(featRaw, (M_1,N_1))
+        N_l = featRawShape[-1]
+        M_l = featRawShape[1]*featRawShape[2]
+        featMatrix = K.reshape(featRaw, (M_l, N_l))
         featMatrix = K.transpose(featMatrix)
         featMatrices.append(featMatrix)
     return featMatrices
@@ -58,11 +61,11 @@ def get_Gram_matrix(F):
 def get_style_loss(ws, Gs, As):
     sLoss = K.variable(0.)
     for w, G, A in zip(ws, Gs, As):
-        M_1 = K.int_shape(G)[1]
-        N_1 = K.int_shape(G)[0]
+        M_l = K.int_shape(G)[1]
+        N_l = K.int_shape(G)[0]
         G_gram = get_Gram_matrix(G)
         A_gram = get_Gram_matrix(A)
-        sLoss+= w*0.25*K.sum(K.square(G_gram - A_gram)) / (N_1**2 * M_1**2)
+        sLoss+= w*0.25*K.sum(K.square(G_gram - A_gram))/ (N_l**2 * M_l**2)
     return sLoss
 
 def get_total_loss(gImPlaceholder, alpha=1.0, beta=10000.0):
@@ -74,65 +77,68 @@ def get_total_loss(gImPlaceholder, alpha=1.0, beta=10000.0):
     return totalLoss
 
 def calculate_loss(gImArr):
+    #Calculate total loss with K.function
     if gImArr.shape != (1, targetWidth, targetWidth, 3):
-        gImArr = gImArr.reshape((1, targetWidth, targetHeight,3))
+        gImArr = gImArr.reshape((1, targetWidth, targetHeight, 3))
     loss_fcn = K.function([gModel.input], [get_total_loss(gModel.input)])
     return loss_fcn([gImArr])[0].astype('float64')
 
 def get_grad(gImArr):
+    #Calculate the gradient of the loss function with respect to the generated image
     if gImArr.shape != (1, targetWidth, targetHeight, 3):
-        gImArr = gImArr.reshape(1, targetWidth, targetHeight,3)
+        gImArr = gImArr.reshape((1, targetWidth, targetHeight, 3))
     grad_fcn = K.function([gModel.input], K.gradients(get_total_loss(gModel.input), [gModel.input]))
     grad = grad_fcn([gImArr])[0].flatten().astype('float64')
     return grad
 
 def postprocess_array(x):
     if x.shape != (targetWidth, targetHeight, 3):
-        x.reshape((targetWidth, targetHeight, 3))
-    x[...,0] += 103.939
-    x[...,1] += 116.779
-    x[...,2] += 123.68
-    #BGR To RGB
+        x = x.reshape((targetWidth, targetHeight, 3))
+    x[..., 0] += 103.939
+    x[..., 1] += 116.779
+    x[..., 2] += 123.68
+    # BGR - RGB
     x = x[..., ::-1]
     x = np.clip(x, 0, 255)
     x = x.astype('uint8')
     return x
 
 def reprocess_array(x):
-    x = np.expand_dims(x.astype('float64'),axis=0)
+    x = np.expand_dims(x.astype('float64'), axis=0)
     x = preprocess_input(x)
     return x
 
-def save_original_size(x, target_size=cImageSizeOrig):
+def save_original_size(x, target_size=baseImageSizeOrig):
     xIm = Image.fromarray(x)
     xIm = xIm.resize(target_size)
-    xIm.save(genImOutputPath)
+    xIm.save(resultPath)
     return xIm
 
 tf_session = K.get_session()
-cModel = VGG16(include_top=False, weights='imagenet', input_tensor=cImArr)
-sModel = VGG16(include_top=False, weights='imagenet', input_tensor=sImArr)
+cModel = VGG16(include_top=False, weights='imagenet', input_tensor=baseImArr)
+sModel = VGG16(include_top=False, weights='imagenet', input_tensor=filterImArr)
 gModel = VGG16(include_top=False, weights='imagenet', input_tensor=gImPlaceholder)
 cLayerName = 'block4_conv2'
 sLayerNames = [
-               'block1_conv1',
-               'block2_conv1',
-               'block3_conv1',
-               'block4_conv1',
-               #'block5_conv1' 
-               ]
+                'block1_conv1',
+                'block2_conv1',
+                'block3_conv1',
+                'block4_conv1',
+                'block5_conv1'
+                ]
 
-P = get_feature_reps(x=cImArr, layer_names=[cLayerName], model=cModel)[0]
-As = get_feature_reps(x=sImArr, layer_names=sLayerNames, model=sModel)
+P = get_feature_reps(x=baseImArr, layer_names=[cLayerName], model=cModel)[0]
+As = get_feature_reps(x=filterImArr, layer_names=sLayerNames, model=sModel)
 ws = np.ones(len(sLayerNames))/float(len(sLayerNames))
 
 iterations = 600
 x_val = gIm0.flatten()
 start = time.time()
-xopt, f_val, info= fmin_l_bfgs_b(calculate_loss, x_val, fprime=get_grad, maxiter=iterations,disp=True)
+xopt, f_val, info= fmin_l_bfgs_b(calculate_loss, x_val, fprime=get_grad,
+                            maxiter=iterations, disp=True)
 xOut = postprocess_array(xopt)
 xIm = save_original_size(xOut)
-print('Img saved')
+print('Image saved')
 end = time.time()
-print("Time taken: ")+(end-start)
+print('Time taken: {}'.format(end-start))
 
